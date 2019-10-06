43b90abee749fa687f4a50009989365e6184ded4
curoky <cccuroky@gmail.com>
2019-10-06 22:12:32 +0800

    [homebrew]: add build.sh and run.sh

---
+++ homebrew/build.sh
@@ -0,0 +1,12 @@
+#!/usr/bin/env bash
+# os: ubuntu:21.04,debian:10
+
+set -xeuo pipefail
+cd "$(dirname $0)" || exit 1
+
+OS=${1-'ubuntu:21.04'}
+OS_SHORT=${OS/:/}
+
+docker buildx build . --network=host --file Dockerfile "${@:2}" \
+  --build-arg OS=${OS} \
+  --tag curoky/homebrew:${OS_SHORT}
